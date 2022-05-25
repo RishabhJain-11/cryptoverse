d506e7abdafe18c8ac9297048675a7a56bce9fee
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-25 22:25:39 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space, Avatar } from 'antd';
 const App = () => {
-  return (
-    <div>
-        <h1>Crypto-Verse</h1> 
-        
-    </div>
-  )
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Avatar src />
+                <Typography.Title level={2} className='logo'>
+                    <Link to="/">
+                        Cryptoverse
+                    </Link>
+                </Typography.Title>
+
+                {/* <Button className='menu-control-container'>
+
+                </Button> */}
+            </div>
+            <div className='main'>
+
+            </div>
+            <div className='footer'>
+
+            </div>
+        </div>
+    )
 }
 
 export default App
