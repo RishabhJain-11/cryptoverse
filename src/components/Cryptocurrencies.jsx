756c2089c11b45ae32f3c72b05c3202c34a28176
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-26 22:16:19 +0530

    ..

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Cryptocurrencies = () => {
   return (
-    <div>Cryptocurrencies</div>
+    <div>
+      
+    </div>
   )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '6220484683msh72b3f1054133c20p1fe247jsna7872d93c0eb'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
