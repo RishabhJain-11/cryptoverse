807f21ec0b66569cf36b089689708faaae671325
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-26 13:26:05 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import store from './app/store';
+// import store from './app/store';
 
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <App />
-      </Provider>
+      {/* </Provider> */}
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
